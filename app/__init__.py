b5d435b054396d7aec4d1ca8e939567527cd8b45
daragmas <80426927+daragmas@users.noreply.github.com>
2025-06-26 20:43:48 -0400

    Initial commit

---
+++ app/__init__.py
@@ -0,0 +1,34 @@
+import os
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
+from data.profiles import profiles
+
+load_dotenv()
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', title="MLH Fellows", url=os.getenv("URL"), profiles=profiles)
+
+
+@app.route('/hobbies/<name>')
+def hobbies(name):
+    profile = next(
+        (p for p in profiles if p['name'].lower().replace(" ", "-") == name), None)
+    return render_template('hobbies.html',
+                           profile=profile,
+                           title=profile['name'],
+                           url=os.getenv("URL"),
+                           profiles=profiles)
+
+
+@app.route('/about/<name>')
+def about_profile(name):
+    profile = next(
+        (p for p in profiles if p['name'].lower().replace(" ", "-") == name), None)
+    return render_template('about_page.html',
+                           profile=profile,
+                           title=profile['name'],
+                           url=os.getenv("URL"),
+                           profiles=profiles)
---
+++ data/profiles.py
@@ -0,0 +1,76 @@
+profiles = [
+    {
+        "name": "Nathaniel Wolf",
+        "profile_picture": "img/profilepic_nate.jpg",
+        "short_bio": "A freelance web developer, with a background in mechanical engineering",
+        "long_bio": "The should be a much longer bio here about Nate, but he put writing off until he was finished "
+                    "with getting the website up and running. So, enjoy a Lorem Ipsum-esque placeholder.",
+        "work_history": [
+            {
+                "position": "AI Quality Control Engineer",
+                "company": "Data Annotation Tech",
+                "years_employed": "2024 - Present"
+            },
+            {
+                "position": "Associate Developer",
+                "company": "BGG Creative Studio",
+                "years_employed": "2023 - Present"
+            },
+            {
+                "position": "Mechanical Engineer",
+                "company": "General Services Administration",
+                "years_employed": "2018 - 2022"
+            },
+        ],
+        "github": "https://github.com/daragmas",
+        "linkedin": "https://www.linkedin.com/in/nathanielpwolf",
+        "email": "natepwolf@gmail.com",
+        "education": {
+            "university": "The College of New Jersey",
+            "major": "Engineering Management",
+            "graduation_year": 2016
+        },
+        "map": "img/nate_visited_map.jpg",
+        "hobbies": [
+            ("TTRPGs", "/img/DnD-Symbol.png"),
+            ("Gardening", "/img/gardening.jpg"),
+            ("Video Games", "/img/steam-logo.jpg")
+        ]
+    },
+    {
+        "name": "Anitha Amarnath",
+        "profile_picture": "img/profilepic_anitha.jpg",
+        "short_bio": "I am a certified Salesforce Developer and a passionate full stack developer with a background in computer science.",
+        "long_bio": "I hold a Master's degree in Information Technology from SRM University and have further enhanced my skills through various certificate programs, including Web Development with Lighthouse Labs, NodeJS with ComIT, and Adobe In-suite with Emily Carr University of Art and Design. I am also a Salesforce Certified Platform Developer I, demonstrating my proficiency and expertise in developing custom applications on the Salesforce platform. I am passionate about continuous learning and staying updated with the latest industry trends and technologies.",
+        "github": "https://github.com/anithaamarnath",
+        "linkedin": "https://linkedin.com/in/anitha-amarnath",
+        "email": "anithaamarnath04@gmail.com",
+        "work_history": [
+            {
+                "position": "Product Developer",
+                "company": "Traction Complete",
+                "years_employed": "2021 - 2023"
+            },
+            {
+                "position": "Wordpress Developer",
+                "company": "Ethony Enterprises",
+                "years_employed": "2020 - 2021"
+            },
+            {
+                "position": "Software Quality Assurance Test Engineer",
+                "company": "Kater Technologies",
+                "years_employed": "2019 - 2019"
+            },
+        ],
+        "education": {
+            "university": "SRM University",
+            "major": "Information Technology",
+            "graduation_year": 2014
+        },
+        "map": "img/anitha_visited_map.png",
+        "hobbies": [
+            ("Travel", "/img/anitha_travel_hobbie.jpg"),
+            ("Chess", "img/anitha_chess_hobbie.jpg"),
+        ]
+    }
+]
