33005485dec2cb84d66d29228c78e388b68165dc
Nate Wolf <daragmas@gmail.com>
2025-07-13 15:42:39 -0400

    added some comments and removed extra line from redeploy script

--- redeploy.sh
+++ redeploy.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# Go into folder
 cd ./week-2-portfolio-deployment/
 
+# Get updates
 git fetch && git reset origin/main --hard
 
-cd ./week-2-portfolio-deployment/
+# Enter virtual env and install requirements
 python3 -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
+
+# Restart Service
 systemctl daemon-reload
 systemctl restart myportfolio
 
