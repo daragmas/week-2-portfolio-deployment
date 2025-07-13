1843200033994aa60c640d70710893935b54c873
Nate Wolf <daragmas@gmail.com>
2025-07-13 15:40:51 -0400

    revised redeploy script

--- redeploy.sh
+++ redeploy.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-tmux kill-sessions
-
 cd ./week-2-portfolio-deployment/
 
 git fetch && git reset origin/main --hard
 
-tmux new-session -d -s 'portfolio'
-tmux send-keys 'cd ./week-2-portfolio-deployment/' C-m
-sleep 1
-tmux send-keys 'python3 -m venv python3-virtualenv' C-m
-sleep 1
-tmux send-keys 'source python3-virtualenv/bin/activate' C-m
-sleep 1
-tmux send-keys 'pip install -r requirements.txt' C-m
-sleep 1
-tmux send-keys 'flask run --host=0.0.0.0' C-m
+cd ./week-2-portfolio-deployment/
+python3 -m venv python3-virtualenv
+source python3-virtualenv/bin/activate
+pip install -r requirements.txt
+systemctl daemon-reload
+systemctl restart myportfolio
+
 echo "deployed"
